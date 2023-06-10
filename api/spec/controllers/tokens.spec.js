b890fbedba5f9cc040a9168fc5d8cde728d26f7d
csanann <csanann@gmail.com>
2023-06-10 19:50:02 +0100

    jest not pass-in review

--- api/spec/controllers/tokens.spec.js
+++ api/spec/controllers/tokens.spec.js
@@ -1,12 +1,16 @@
+//api/spec/controllers/tokens.spec.js
+
 const app = require("../../app");
 const request = require("supertest");
 require("../mongodb_helper");
 const User = require('../../models/user');
 
-describe("/tokens", () => {
-  beforeAll( () => {
-    const user = new User({ email: "test@test.com", password: "12345678" })
-    user.save()
+describe("/login", () => {
+  beforeAll(async () => {
+    const user = new User({ name: "Candy Duck", email: "test@test.com", password: "12345678" });//name: "Test User", 
+    await user.save();
+    //asynch operation that return a promise so we've to wait for the completion before running test cases
+    //or else test cases might run before the user has been saved to DB
   });
 
   afterAll( async () => {
@@ -14,21 +18,25 @@ describe("/tokens", () => {
   })
 
   test("a token is returned when creds are valid", async () => {
-    let response = await request(app)
-      .post("/tokens")
-      .send({email: "test@test.com", password: "12345678"})
-    expect(response.status).toEqual(201)
+    const response = await request(app)
+      .post("/users/login")
+      .send({name: "Candy Duck", email: "test@test.com", password: "12345678"})
+    expect(response.status).toBe(200)
     expect(response.body.token).not.toEqual(undefined)
-    expect(response.body.message).toEqual("OK")
-  })
+    // expect(response.body.message).toEqual("OK") <prev
+    //modify expected msg string
+    expect(response.body.message).toEqual("User logged in successfully")
+  });
 
 
   test("a token is not returned when creds are invalid", async () => {
-    let response = await request(app)
-      .post("/tokens")
-      .send({email: "test@test.com", password: "1234"})
-    expect(response.status).toEqual(401)
+    const response = await request(app)
+      .post("/users/login") 
+      .send({name: "Candy Duck", email: "test@test.com", password: "wrongpassword"})
+    expect(response.status).toBe(401)
     expect(response.body.token).toEqual(undefined)
-    expect(response.body.message).toEqual("auth error")
-  })
-})
+    //modify expected msg string
+    expect(response.body.error).toEqual("Invalid email or password, Please Try again!")
+    // expect(response.body.message).toEqual("auth error")
+  });
+});
