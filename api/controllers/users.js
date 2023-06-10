ea94d79a68e9c368730ac764976c00448d89ad78
csanann <csanann@gmail.com>
2023-06-10 19:44:40 +0100

    Refactor done, jest pass

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,6 +4,34 @@
 //the controller is not utilized yet but kept for potential future use and for adhering to mvc pattern
 
 // api/controllers/users.js
+//===========================
+// const User = require("../models/User");
+// const bcrypt = require("bcrypt");
+
+// class UsersController {
+//   //signupUser is an asynchronous method that handles user signup/registration
+//   static async signup(req, res) {
+//     try {
+//       const hashedPassword = await bcrypt.has(req.body.password, 10);//hash pw
+//        //if a user doesn't exists, create a new one and save it in DB
+//       const user = new User({
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: hashedPassword, //saved hashed pw
+//       });
+
+//       await user.save();
+//       res.status(201).send({ message: "User signed up successfully!" });
+//     } catch (error) {
+//       if (error.code === 11000) {
+//         res.status(409).json({ error: 'Email already exists' });
+//       } else {
+//         res.status(500).json({ error: 'Internal server error' });
+//       }  
+//     }
+//   }
+// }
+//========================
 
 const User = require("../models/User");
 
@@ -22,21 +50,22 @@ class UsersController {
       res.status(500).json({ error: 'Internal server error' });
     }
   }
-
-  static async loginUser(req, res) {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (!user || !await user.isValidPassword(password)) {
-        return res.status(401).json({ error: 'Invalid email or password, Please Try again!'});
-      }
-      // Generate and send authentication token
-      // ...
-      res.status(200).json({ message: 'User logged in successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  }
 }
+//   static async loginUser(req, res) {
+//     try {
+//       const { email, password } = req.body;
+//       const user = await User.findOne({ email });
+//       if (!user || !await user.isValidPassword(password)) {
+//         return res.status(401).json({ error: 'Invalid email or password, Please Try again!'});
+//       }
+//       // Generate and send authentication token
+//       // ...
+//       res.status(200).json({ message: 'User logged in successfully' });
+//     } catch (error) {
+//       res.status(500).json({ error: 'Internal server error' });
+//     }
+//   }
+// }
 
 module.exports = UsersController;
+
