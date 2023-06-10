b52619ae267c370b5b6cf845d38cd68b56c3daac
csanann <csanann@gmail.com>
2023-06-10 16:36:47 +0100

    Refactor done,jest pass

--- api/models/token_generator.js
+++ api/models/token_generator.js
@@ -1,15 +1,23 @@
+// api/models/token_generator.js
+require('dotenv').config(); //loading envir variables
 const JWT = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
+// if (!secret) {
+//   console.error("Missing JWT_SECRET environment variable. Shutting down..");
+//   process.exit(1);
+// } for debug log
+
 class TokenGenerator {
   static jsonwebtoken(user_id) {
-    return JWT.sign({
+    const secret = process.env.JWT_SECRET;
+    return JWT.sign(
+      {
       user_id: user_id,
-      iat: Math.floor(Date.now() / 1000),
-      
+      iat: Math.floor(Date.now() / 1000),  
       // Set the JWT token to expire in 10 minutes
-      exp: Math.floor(Date.now() / 1000) + (10 * 60)
-    }, secret);
+      // exp: Math.floor(Date.now() / 1000) + (10 * 60)
+    }, secret, { expiresIn: '10m'});
   }
 }
 
