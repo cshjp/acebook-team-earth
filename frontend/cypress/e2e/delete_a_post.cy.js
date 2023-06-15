5cdcd8426695ac807a0eacfbb9de37da63833be1
faridat <fa385@cantab.ac.uk>
2023-06-15 20:15:01 +0100

    backend completed

--- frontend/cypress/e2e/delete_a_post.cy.js
+++ frontend/cypress/e2e/delete_a_post.cy.js
@@ -1,21 +1,17 @@
-describe('Deleting a post', () => {
-    it('signs up', () => {
-      cy.signup("update@test.com", "update")
-    })
-  
-    it('should make a post when user is logged in', () => {
-      cy.login("update@test.com", "update");
-      cy.url().should("include", "/posts");
-      cy.visit('/create-post');
-      cy.url().should("include", "/create-post");
-      cy.get('#message').type('quack-test');
-      cy.get('#submit').click();
-      cy.url().should("include", "/posts");
-      cy.contains('quack-test');
-      cy.get('#delete-link').click();
-      
-      cy.get('#submit').click();
-      cy.url().should("include", "/posts");
-      cy.contains('quack-test').should('not.exist');
-    })
-  })
+beforeEach(() => {
+  cy.signup("duck@pond.com", "Il0veBread")
+  cy.login("duck@pond.com", "Il0veBread")
+ });
+ it('should make two posts and then delete the first one, () => {
+  cy.visit('/create-post');
+  cy.get('#message').type('duck');
+  cy.get('#submit').click();
+  cy.visit('/create-post');
+  cy.get('#message').type('goose');
+  cy.get('#submit').click();
+  cy.contains('duck');
+  cy.contains('goose');
+  cy.visit(‘/delete-post’); // I think we still need to define this, and perhaps we need to edit the command.js file too?
+  cy.contains('goose');
+ })
+});
